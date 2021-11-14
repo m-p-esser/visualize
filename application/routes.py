4cb8fe01992da8f121440f48bb11561208f55926
Marc-Philipp Esser <m-esser@mail.de>
2021-11-14 18:26:27 +0100

    Add route to show Diagram pages, first experiments with passing variables via route

--- application/routes.py
+++ application/routes.py
@@ -21,3 +21,38 @@ def about():
 def seaborn():
    title = "Seaborn"
    return render_template("seaborn.html", title=title)
+
+@app.route('/<diagram_name>')
+def diagram(diagram_name):
+   diagram_name_formatted = diagram_name.replace('_', ' ').capitalize()
+   return render_template(
+      "diagram.html", 
+      diagram_name=diagram_name,
+      diagram_name_formatted=diagram_name_formatted
+      )
+
+# @app.errorhandler(404)
+# def not_found():
+#     """Page not found."""
+#     return make_response(
+#         render_template("404.html"),
+#         404
+#      )
+
+
+# @app.errorhandler(400)
+# def bad_request():
+#     """Bad request."""
+#     return make_response(
+#         render_template("400.html"),
+#         400
+#     )
+
+
+# @app.errorhandler(500)
+# def server_error():
+#     """Internal server error."""
+#     return make_response(
+#         render_template("500.html"),
+#         500
+#     )
